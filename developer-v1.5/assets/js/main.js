555921d1f2ed9678ecf2343a03dff6841c10d604
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2018-03-18 18:54:18 +0100

    update

--- developer-v1.5/assets/js/main.js
+++ developer-v1.5/assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar(".calendar-graph", "vamsiDT");
+    GitHubCalendar({container: ".calendar-graph", username: "vamsiDT"});
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
