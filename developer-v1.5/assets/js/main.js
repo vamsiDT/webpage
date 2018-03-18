6f106b6911f72dd27c5941ed46f7aa178501f76b
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2018-03-18 19:22:41 +0100

    update

--- developer-v1.5/assets/js/main.js
+++ developer-v1.5/assets/js/main.js
@@ -59,7 +59,7 @@ jQuery(document).ready(function($) {
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "vamsiDT", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "vamsi", selector: "#ghfeed" });
 
 
 });
